1cffe1e6c1767dda5866e43486622358b20f57cd
istefanovics <spider00720@gmail.com>
2022-05-11 21:22:23 +0200

    Public init

--- spm_test/test.swift
+++ spm_test/test.swift
@@ -8,6 +8,8 @@
 import Foundation
 
 public class TestClass {
+    public init() {}
+    
     public func testCall(completion: @escaping (String) -> ()?) {
         testCallInternal { result in
             completion(result)
